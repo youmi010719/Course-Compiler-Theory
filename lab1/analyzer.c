663fc59d880a9f93ff9745d0847f698c4a9c8ba1
Autzoko <llt020725@163.com>
2022-10-20 15:22:02 +0800

    base

--- lab1/analyzer.c
+++ lab1/analyzer.c
@@ -1,4 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include "analyzer_args.h"
---
+++ lab1/analyzer_args.h
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define IDENTIFIER 		1		//identifier
+#define UNSIGNED_INT 	2		//unsigned integer
+#define VOID 			3		//void
+#define INT 			4		//int
+#define FLOAT 			5		//float
+#define DOUBLE 			6		//double
+#define IF 				7		//if
+#define ELSE 			8		//else
+#define FOR 			9		//for
+#define DO 				10		//do
+#define WHILE 			11		//while
+#define RETURN 			12		//return
+#define BREAK 			13		//break
+
+#define ADD 			14		// +
+#define MIN 			15		// -
+#define MUL 			16		// *
+#define SEMICOLON 		17		// ;
+#define COMMA 			18		// ,
+#define L_PTS 			19		// (
+#define R_PTS 			20		// )
+#define L_BRACKET 		21		// [
+#define R_BRACKET 		22		// ]
+#define L_BRACE 		23		// {
+#define R_BRACE 		24		// }
+#define GT 				25		// >
+#define LT 				26		// <
+#define MOV 			27		// =
+#define NOT 			28		// !
+#define DIV 			29		// /
+#define GE 				30		// >=
+#define LE 				31		// <=
+#define NE 				32		// !=
+#define EQU 			33		// ==
+#define IDIV 			34		// %
+#define SADD 			35		// ++
+#define SMIN 			36		// --
+#define AADD 			37		// +=
+#define AMIN			38		// -=
+#define AMUL			39		// *=
+#define ADIV 			40		// /=
+#define LMOV 			41		// >>
+#define RMOV 			42		// <<
+#define AND 			43		// &&
+#define OR 				44		// ||
+#define COL 			45		// |
+#define SAND 			46		// &
